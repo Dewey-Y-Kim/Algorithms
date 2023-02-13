8609d6d165df85c4ca170da829251bcabd99ba23
youth <dcancer17@gmail.com>
2023-02-13 09:04:05 +0900

    230213

--- Algorithms/src/Programer/AilianDic.java
+++ Algorithms/src/Programer/AilianDic.java
@@ -1,25 +1,32 @@
 package Programer;
 
+import java.util.Arrays;
+
 public class AilianDic {
 
 	public AilianDic() {
 		// TODO Auto-generated constructor stub
 	}
 	public int solution(String[] spell, String[] dic) {
-		int answer=2;
-		String str="";
-		for (String s:dic) {
-			str+=s+" ";
+		String word="";
+		int answer =0;
+		for(String str : spell) {
+			word+=str;
 		}
-		for(String s:spell) {
-			str=str.replaceAll(s, "");
+		for(String str:dic) {
+			String[] dictionary = str.split("");
+			boolean result=true;
+			for (String s : dictionary) {
+				int i=0;
+				if(word.indexOf(s)<0){
+					result=false;
+				}
+			}
+			if(result) {
+				answer++;
+			}
 		}
-		spell=str.split(" ");
-		for(String s:spell) {
-			answer*=s.length();
-		}
-		if (answer==0) {return 1;}
-		else {return 2;}
+		return answer;
 	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -27,7 +34,7 @@ public class AilianDic {
 		String[] spell= {"s", "o", "m", "d"};
 		
 		System.out.println(new AilianDic().solution(spell,dic));
-		System.out.println(0*0);
+		
 	}
 
 }
--- Algorithms/src/Programer/polynomial.java
+++ Algorithms/src/Programer/polynomial.java
@@ -5,32 +5,38 @@ public class polynomial {
 	public polynomial() {
 		// TODO Auto-generated constructor stub
 	}
-	dpublic String solution(String polynomial) {
-	        polynomial=polynomial.replaceAll(" ","");
-	        
-	    	String[] position =polynomial.split("\\+");
-	    	for(String str:position) {
-	    	System.out.print(str+"\t");
-	    	}
-	    	int x=0, n=0;
-	    	for(String str:position) {
-	    		switch(str.indexOf("x")){
-	    			case -1 : n+=Integer.parseInt(str); break;
-	    			case 0: x++; break;
-	    			default : x+=Integer.parseInt(str.substring(0, str.indexOf("x"))); break;
-	    		}
-	    	}
-	        String answer="";
-	    	if(n==0) {
-	        	answer=String.valueOf(x);
-	    	}else{
-	    		answer=String.valueOf(x)+n;
-	        }
-	    	return answer;
-	    }
+	public String solution(String polynomial) {
+		polynomial=polynomial.replaceAll(" ","");
+		
+		String[] position =polynomial.split("\\+");
+		int x=0, n=0;
+		for(String str:position) {
+			switch(str.indexOf("x")){
+			case -1 : n+=Integer.parseInt(str); break;
+			case 0: x++; break;
+			default : x+=Integer.parseInt(str.substring(0, str.indexOf("x"))); break;
+			}
+		
+		String answer="";
+		if(n==0) {
+			if(x==0) {
+				answer="n";
+			}else if(x==1) {
+				answer=String.valueOf(x)+"x";
+			}
+		}else {
+			if(x==0) {
+				answer="n";
+			}else if(x==1) {
+				answer=String.valueOf(x)+"x + "+n;
+			}
+		}
+        return answer;
+		}    
+}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String x="1234x";
+		String x="3x + 7 + x";
 		System.out.println(new polynomial().solution(x));
 		System.out.println(1+x.substring(0,4));
 	}
