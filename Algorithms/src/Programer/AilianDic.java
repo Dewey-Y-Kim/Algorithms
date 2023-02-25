dfb2f43721ef4c8202b0f64277da169b5d19e6e6
youth <dcancer17@gmail.com>
2023-02-25 18:05:46 +0900

    230225

--- Algorithms/src/Programer/AilianDic.java
+++ Algorithms/src/Programer/AilianDic.java
@@ -1,39 +1,34 @@
 package Programer;
 
-import java.util.Arrays;
-
 public class AilianDic {
 
 	public AilianDic() {
 		// TODO Auto-generated constructor stub
 	}
-	public int solution(String[] spell, String[] dic) {
-		String word="";
-		int answer =0;
-		for(String str : spell) {
-			word+=str;
-		}
-		for(String str:dic) {
-			String[] dictionary = str.split("");
-			boolean result=true;
-			for (String s : dictionary) {
-				int i=0;
-				if(word.indexOf(s)<0){
-					result=false;
-				}
-			}
-			if(result) {
-				answer++;
-			}
-		}
-		return answer;
-	}
+	 public int solution(String[] spell, String[] dic) {
+	        int answer = 2;
+	        boolean result=false;
+	        for(String check :dic) {
+	        	for (String str:spell) {
+	        		if(check.indexOf(str)>=0&&check.indexOf(str)==check.lastIndexOf(str)) {
+	        			result=true;
+	        		} else {
+	        			result=false;
+	        			break;
+	        		}
+	        	}
+	        	if(result) {
+	        		answer=1;
+	        	}
+	        	System.out.print(check+"\t"+result+"\n");
+	        }
+	        return answer;
+	    }
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		String[] dic= {"moos", "dzx", "smm", "sunmmo", "som"};
 		String[] spell= {"s", "o", "m", "d"};
-		
-		System.out.println(new AilianDic().solution(spell,dic));
+		new AilianDic().solution(spell,dic);
 		
 	}
 
