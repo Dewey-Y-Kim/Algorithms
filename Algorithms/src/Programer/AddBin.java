5845bae69a31fbef53a8aa14fab4b3ca0f2438fe
youth <dcancer17@gmail.com>
2023-02-28 19:23:27 +0900

    230228

--- Algorithms/src/Programer/AddBin.java
+++ Algorithms/src/Programer/AddBin.java
@@ -5,7 +5,7 @@ public class AddBin {
 	public AddBin() {}
 	public String solution(String bin1, String bin2) {
 		// 편법 말고 정법 준비중
-		St ring answer="";
+		String answer="";
 		String[] num1= new String[bin1.length()];
 		String[] num2= new String[bin2.length()];
 		String[] sol = new String[num1.length+1];
@@ -18,24 +18,16 @@ public class AddBin {
 				}else{ sol[i+1]="0";}
 			}else{ sol[i+1]="1";}
 		}
-		for(int i=num1.length; i>=1; i--) {
-			if(sol[i]=="c") {
-				if(num1[i].equals(num2[i])){
-					if(num1[i].equals("1")){
-						sol[i]="c";
-					}else{ sol[i]="0";}
-				}else{ sol[i]="1";}
+		boolean result=true;
+		for (int i=sol.length-1;i>0;i++) {
+			if(sol[i].equals("c")){
+				sol[i]="1";
+				if(sol[i-1].equals("0")) {sol[i-1]="1";}
+				{else if(sol[i-1].equals("1")){sol[i-1]="c";}
+				
 			}
 		}
-		if(sol[0]=="0") {
-			for(int i=1;i<sol.length;i++) {
-				answer+=sol[i];
-			}
-		}else {
-			for(String str : sol) {
-				answer+=str;
-			}
-		}
+		
 		return answer;
 	}
 	
