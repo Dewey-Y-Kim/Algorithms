ae48e1c86bb4d9de4e2469c7080e6c5716b9b16f
youth <dcancer17@gmail.com>
2023-03-05 17:47:55 +0900

    230305

---
+++ Algorithms/src/Backjoon/Back1001.java
@@ -0,0 +1,13 @@
+package Backjoon;
+
+import java.util.Scanner;
+public class Back1001{
+    public static void main(String Args[]){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println(a+b);
+        sc.close();
+    }
+}
+
